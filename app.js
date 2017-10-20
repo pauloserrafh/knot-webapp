893aa9271e885753d0f2847eb810bc3a89479260
Rodrigo Alves <raan2@cesar.org.br>
2017-10-20 17:48:31 -0300

    SetData: backend implementation

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var getDevices = require('./routes/getDevices');
 var httpGetDevices = require('./routes/httpGetDevices');
+var setData = require('./routes/setData');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/getDevices', getDevices);
 app.use('/httpGetDevices', httpGetDevices);
+app.use('/setData', setData);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
