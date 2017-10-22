b5ce2fe896d2dca1efb6ccf4ca9e626b9390b381
Rodrigo Alves <raan2@cesar.org.br>
2017-10-21 21:30:53 -0300

    Subscribe operation

--- app.js
+++ app.js
@@ -11,44 +11,46 @@ var httpGetDevices = require('./routes/httpGetDevices');
 var setData = require('./routes/setData');
 var getData = require('./routes/getData');
 var sendConfig = require('./routes/sendConfig');
+var subscribe = require("./routes/subscribe");
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', index);
-app.use('/getDevices', getDevices);
+app.use("/", index);
+app.use("/getDevices", getDevices);
 app.use('/httpGetDevices', httpGetDevices);
-app.use('/setData', setData);
-app.use('/getData', getData);
-app.use('/sendConfig', sendConfig);
+app.use("/setData", setData);
+app.use("/getData", getData);
+app.use("/sendConfig", sendConfig);
+app.use("/subscribe", subscribe);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error("Not Found");
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
